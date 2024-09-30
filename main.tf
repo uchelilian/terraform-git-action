d1b72d6fc7a0926b9bb7dd89a190cc91a5ab02df
uchelilian <72115396+uchelilian@users.noreply.github.com>
2024-09-30 10:09:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1" # define region as per your account
 }
 
-resource "aws_s3_bucket" "new_bucket" {
+resource "aws_s3_bucket" "git-action-demo-bucket" {
   bucket = "demo-github-action-tf-medium"
 
   object_lock_enabled = false
