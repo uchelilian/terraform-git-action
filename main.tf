5aa6dd92ea6ad6df2c3625850b5fd06b2e388f2f
uchembaeyi <84679129+uchembaeyi@users.noreply.github.com>
2024-09-30 11:26:15 -0400

    removed action on push to main

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1" # define region as per your account
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "new_bucket" {
